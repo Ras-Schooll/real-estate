9a79aac1ab79ce1f145bca01239eab551bc8af03
ras-schooll <wilfredchilunga7@gmail.com>
2023-11-03 11:28:50 +0200

    Create Sign-Up API route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
  
 dotenv.config();
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 
 const app = express();
+app.use(express.json());
 
 const PORT = '3001';
 
@@ -22,3 +24,4 @@ app.listen(PORT, () => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
